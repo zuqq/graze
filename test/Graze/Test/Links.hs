8ac7df479da0ca1a9d499782201f31d58e0c2d48
zuqq <noahheld17@gmail.com>
2020-08-11 09:44:33 +0200

    Add a test case for `Links`

--- test/Graze/Test/Links.hs
+++ test/Graze/Test/Links.hs
@@ -4,23 +4,30 @@ module Graze.Test.Links
     ( tests
     ) where
 
-import Test.Tasty       (TestTree, testGroup)
-import Test.Tasty.HUnit ((@?=), testCase)
+import qualified Data.HashSet     as H (fromList)
+import           Test.Tasty       (TestTree, testGroup)
+import           Test.Tasty.HUnit ((@?=), testCase)
 
 import Graze.HttpUrl (HttpUrl (HttpUrl))
 import Graze.Links   (links)
 
 testLinks :: TestTree
 testLinks = testCase "links" $ do
-    links base (header <> a <> footer)
-        @?= [HttpUrl "http:" "//www.example.com" "/a"]
-    links base (header <> b <> footer)
-        @?= [HttpUrl "http:" "//www.example.com" "/a/b"]
-    links base (header <> haskell <> footer)
-        @?= [HttpUrl "http:" "//www.haskell.org" "/"]
+    H.fromList (links base $ header <> a <> footer)
+        @?= H.fromList [HttpUrl "http:" "//www.example.com" "/a"]
+    H.fromList (links base $ header <> b <> footer)
+        @?= H.fromList [HttpUrl "http:" "//www.example.com" "/a/b"]
+    H.fromList (links base $ header <> haskell <> footer)
+        @?= H.fromList [HttpUrl "http:" "//www.haskell.org" "/"]
+    H.fromList (links base $ header <> a <> b <> haskell <> footer)
+        @?= H.fromList
+                [ HttpUrl "http:" "//www.example.com" "/a"
+                , HttpUrl "http:" "//www.example.com" "/a/b"
+                , HttpUrl "http:" "//www.haskell.org" "/"
+                ]
   where
     base = HttpUrl "http:" "//www.example.com" "/"
-    header = "<!doctype html><body>"
+    header = "<!DOCTYPE html><body>"
     footer = "</body>"
     a = "<a href=\"a\">a</a>"
     b = "<a href=\"a/b\">b</a>"

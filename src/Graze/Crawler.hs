bd859ee62cad4ed1f45d10c9de3625d7c8936e5d
zuqq <noahheld17@gmail.com>
2020-08-28 21:17:59 +0200

    Bring in some lenses

--- src/Graze/Crawler.hs
+++ src/Graze/Crawler.hs
@@ -13,12 +13,16 @@ import Control.Monad.IO.Class           (MonadIO (liftIO))
 import Control.Monad.Trans.State.Strict (StateT, evalStateT, get, gets, modify', put)
 import Data.Foldable                    (foldl', traverse_)
 
-import qualified Data.HashSet as HS (HashSet, insert, member, singleton)
+import qualified Data.HashSet   as HS (HashSet, insert, member, singleton)
+import           Lens.Micro     (Lens')
+import           Lens.Micro.Mtl ((+=), (-=), (.=), use)
 
 import Graze.HttpUrl  (HttpUrl)
 import Graze.Messages
 import Graze.Util     (readFrom, writeTo, writeManyTo)
 
+
+-- process ---------------------------------------------------------------------
 
 data Triple a b c = Triple !a !b !c
 
@@ -36,16 +40,28 @@ process s xs = done $ foldl' step (Triple s 0 id) xs
         else Triple (x `HS.insert` s') (i + 1) ((x :) . ys)
     done (Triple s' i ys)   = (s', i, ys [])
 
+-- CrawlerConfig ---------------------------------------------------------------
+
 data CrawlerConfig = CrawlerConfig
     { base  :: !HttpUrl            -- ^ Base URL.
     , depth :: !Int                -- ^ Depth of the search.
     , legal :: !(HttpUrl -> Bool)  -- ^ Predicate for selecting URLs to crawl.
     }
 
+-- CrawlerState ----------------------------------------------------------------
+
 data CrawlerState = CrawlerState
-    { seen :: !(HS.HashSet HttpUrl)
-    , open :: !Int
+    { _seen :: !(HS.HashSet HttpUrl)
+    , _open :: !Int
     }
+
+seen :: Lens' CrawlerState (HS.HashSet HttpUrl)
+seen p (CrawlerState s i) = fmap (\s' -> CrawlerState s' i) (p s)
+
+open :: Lens' CrawlerState Int
+open q (CrawlerState s i) = fmap (\i' -> CrawlerState s i') (q i)
+
+-- Crawler ---------------------------------------------------------------------
 
 type Crawler a = StateT CrawlerState IO a
 
@@ -63,11 +79,12 @@ runCrawler CrawlerConfig {..} Chans {..} = do
             Success Job {..} links body -> do
                 writeTo writerChan $ Write (Record origin url links) body
                 unless (depth <= 0) $ do
-                    CrawlerState s n <- get
+                    s <- use seen
                     let (s', i, links') = process s . filter legal $ links
                     writeManyTo fetcherChan
                         [Fetch (Job url link (depth - 1)) | link <- links']
-                    put $! CrawlerState s' (n + i)
-        n <- gets open
-        modify' $ \s -> s {open = n - 1}
-        unless (n <= 1) loop
+                    seen .= s'
+                    open += i
+        open -= 1
+        n <- use open
+        unless (n <= 0) loop

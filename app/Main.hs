3b809fcb9f1d37b0901a73fef3baadf11438bc06
zuqq <noahheld17@gmail.com>
2020-11-19 13:29:24 +0100

    Qualified import of `optparse-applicative`

--- app/Main.hs
+++ app/Main.hs
@@ -2,36 +2,38 @@ module Main
     ( main
     ) where
 
-import qualified Data.Text as T (pack)
+import           Control.Applicative ((<**>))
+import qualified Data.Text           as T (pack)
 
-import Options.Applicative
+import qualified Options.Applicative as O
 
 import Graze (Config (Config), parseUrl, run)
 
-parser :: Parser Config
+
+parser :: O.Parser Config
 parser = Config
-    <$> argument
-        (eitherReader $ parseUrl . T.pack)
-        (metavar "<URL>" <> help "URL to start at")
-    <*> argument
-        str
-        (metavar "<folder>" <> help "Download folder")
-    <*> option
-        auto
-        (long "depth"
-            <> metavar "<depth>"
-            <> value 3
-            <> showDefault
-            <> help "Depth of the search")
-    <*> option
-        auto
-        (long "threads"
-            <> metavar "<threads>"
-            <> value 10
-            <> showDefault
-            <> help "Number of threads")
+    <$> O.argument
+        (O.eitherReader $ parseUrl . T.pack)
+        (O.metavar "<URL>" <> O.help "URL to start at")
+    <*> O.argument
+        O.str
+        (O.metavar "<folder>" <> O.help "Download folder")
+    <*> O.option
+        O.auto
+        (O.long "depth"
+            <> O.metavar "<depth>"
+            <> O.value 3
+            <> O.showDefault
+            <> O.help "Depth of the search")
+    <*> O.option
+        O.auto
+        (O.long "threads"
+            <> O.metavar "<threads>"
+            <> O.value 10
+            <> O.showDefault
+            <> O.help "Number of threads")
 
 main :: IO ()
 main = do
-    config <- execParser $ info (parser <**> helper) fullDesc
+    config <- O.execParser $ O.info (parser <**> O.helper) O.fullDesc
     run config

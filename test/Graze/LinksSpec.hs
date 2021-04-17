b92efba894550e8e5393ca2bcf0d0f3f399c9c04
zuqq <noahheld17@gmail.com>
2021-04-17 21:18:15 +0200

    Make `LinksSpec` and `RobotsSpec` more idiomatic

--- test/Graze/LinksSpec.hs
+++ test/Graze/LinksSpec.hs
@@ -1,37 +1,35 @@
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Graze.LinksSpec (spec) where
 
-import Data.Foldable (for_)
 import Test.Hspec
-
-import qualified Data.ByteString.Lazy as Lazy
-import qualified Data.HashSet as HashSet
 
 import Graze.Links
 import Graze.Url
 
-valid :: [(Url, Lazy.ByteString, [Url])]
-valid =
-    [ ( Url "http:" "//www.example.com" "/"
-      , "<!DOCTYPE html><body><a href=a>a</a></body>"
-      , [Url "http:" "//www.example.com" "/a"]
-      )
-    , ( Url "http:" "//www.example.com" "/"
-      , "<!DOCTYPE html><body><a href='a'>a</a></body>"
-      , [Url "http:" "//www.example.com" "/a"]
-      )
-    , ( Url "http:" "//www.example.com" "/"
-      , "<!DOCTYPE html><body><a href=\"a\">a</a></body>"
-      , [Url "http:" "//www.example.com" "/a"]
-      )
-    ]
-
 parseLinksSpec :: Spec
-parseLinksSpec = describe "valid examples" $
-    for_ valid $ \(base, bs, urls) ->
-        specify (show bs) $
-            HashSet.fromList (parseLinks base bs) `shouldBe` HashSet.fromList urls
+parseLinksSpec = do
+    describe "valid examples" do
+        it "handles lack of input gracefully" do
+            parseLinks base ""
+                `shouldBe` []
+        it "finds links with no quotes" do
+            parseLinks base "<!DOCTYPE html><body><a href=a>a</a></body>"
+                `shouldBe` links
+        it "finds links with single quotes" do
+            parseLinks base "<!DOCTYPE html><body><a href='a'>a</a></body>"
+                `shouldBe` links
+        it "finds links with double quotes" do
+            parseLinks base "<!DOCTYPE html><body><a href=\"a\">a</a></body>"
+                `shouldBe` links
+    describe "invalid examples" do
+        it "ignores malformed links" do
+            parseLinks base "<!DOCTYPE html><body><ahref=a>a</a></body>"
+                `shouldBe` []
+  where
+    base = Url "http:" "//www.example.com" "/"
+    links = [Url "http:" "//www.example.com" "/a"]
 
 spec :: Spec
 spec = describe "parseLinks" parseLinksSpec
--- test/Graze/RobotsSpec.hs
+++ test/Graze/RobotsSpec.hs
@@ -1,57 +1,43 @@
+{-# LANGUAGE BlockArguments    #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Graze.RobotsSpec (spec) where
 
-import Data.Foldable (for_)
-import Data.Text (Text)
 import Test.Hspec
-
-import qualified Data.Text as Text
 
 import Graze.Robots
 
-content :: Text
-content = Text.unlines
-    [ "# /robots.txt for http://www.fict.org/"
-    , "# comments to webmaster@fict.org"
-    , ""
-    , "User-agent: unhipbot"
-    , "Disallow: /"
-    , ""
-    , "User-agent: webcrawler"
-    , "User-agent: excite"
-    , "Disallow: "
-    , ""
-    , "User-agent: *"
-    , "Disallow: /"
-    , "Allow: /org/"
-    , "Allow: /serv"
-    ]
-
-paths :: [Text]
-paths =
-    [ "/"
-    , "/index.html"
-    , "/services/fast.html"
-    , "/orgo.gif"
-    , "/org/about.html"
-    ]
-
-cases :: [(UserAgent, [Bool])]
-cases =
-    [ ("unhipbot"  , [False, False, False, False, False])
-    , ("webcrawler", [True , True , True , True , True ])
-    , ("excite"    , [True , True , True , True , True ])
-    , ("googlebot" , [False, False, True , False, True ])
-    ]
-
 parseRobotsSpec :: Spec
-parseRobotsSpec = describe "valid examples" $
-    for_ cases $ \(userAgent, results) ->
-        describe (show userAgent) $
-            let legal = parseRobots userAgent content
-            in for_ (zip paths results) $ \(path, result) ->
-                specify (show path) $ legal path `shouldBe` result
+parseRobotsSpec = do
+    describe "valid example" do
+        it "parses rules for unhipbot correctly" do
+            or (fmap (parseRobots "unhipbot" s) paths)
+                `shouldBe` False
+        it "parses rules for webcrawler correctly" do
+            and (fmap (parseRobots "webcrawler" s) paths)
+                `shouldBe` True
+        it "parses rules for excite correctly" do
+            and (fmap (parseRobots "excite" s) paths)
+                `shouldBe` True
+        it "parses rules for googlebot correctly" do
+            fmap (parseRobots "googlebot" s) paths
+                `shouldBe` [False, False, True, False, True]
+  where
+    s = "# /robots.txt for http://www.fict.org/\n\
+        \# comments to webmaster@fict.org      \n\
+        \                                      \n\
+        \User-agent: unhipbot                  \n\
+        \Disallow: /                           \n\
+        \                                      \n\
+        \User-agent: webcrawler                \n\
+        \User-agent: excite                    \n\
+        \Disallow:                             \n\
+        \                                      \n\
+        \User-agent: *                         \n\
+        \Disallow: /                           \n\
+        \Allow: /org/                          \n\
+        \Allow: /serv                          \n"
+    paths = ["/", "/index.html", "/services/fast.html", "/orgo.gif", "/org/about.html"]
 
 spec :: Spec
 spec = describe "parseRobots" parseRobotsSpec
